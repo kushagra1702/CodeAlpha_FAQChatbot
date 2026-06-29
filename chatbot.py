e1c9ba505c92b08e786c30cecb665c4e25fdfba4
kushagra1702 <mkushagra179@gmail.com>
2026-06-29 12:30:30 +0530

    Complete Smart FAQ Chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,24 +1,66 @@
+
 from difflib import get_close_matches
 from faq_data import faq_data
-
-print("=" * 55)
-print("🤖 CodeAlpha FAQ Chatbot")
-print("=" * 55)
+from datetime import datetime
 
 questions = list(faq_data.keys())
 
+questions_asked = 0
+answers_found = 0
+not_found = 0
+
+print("="*60)
+print("🤖 CodeAlpha Smart FAQ Chatbot")
+print("="*60)
+print("Type 'exit' anytime to quit.\n")
+
 while True:
 
-    user_question = input("\nAsk your question (or type 'exit' to quit):\n")
+    user_question = input("You : ")
 
     if user_question.lower() == "exit":
-        print("\n👋 Thank you for using the FAQ Chatbot!")
         break
 
-    match = get_close_matches(user_question, questions, n=1, cutoff=0.4)
+    questions_asked += 1
+
+    match = get_close_matches(user_question, questions, n=3, cutoff=0.4)
 
     if match:
-        print("\n✅ Answer:")
-        print(faq_data[match[0]])
+
+        answer = faq_data[match[0]]
+
+        print("\n🤖", answer)
+
+        answers_found += 1
+
+        try:
+            with open("chat_history.txt", "a", encoding="utf-8") as file:
+                file.write("="*60 + "\n")
+                file.write("Time : " + str(datetime.now()) + "\n")
+                file.write("Question : " + user_question + "\n")
+                file.write("Matched FAQ : " + match[0] + "\n")
+                file.write("Answer : " + answer + "\n")
+                file.write("="*60 + "\n\n")
+        except Exception as e:
+            print("\n⚠ Unable to save chat history.")
+            print(e)
+
     else:
-        print("\n❌ Sorry, I couldn't find an answer for that question.")
+
+        print("\n❌ Sorry, I couldn't find an answer.")
+
+        if len(match) > 0:
+            print("\nDid you mean:")
+            for item in match:
+                print("•", item)
+
+        not_found += 1
+
+print("\n" + "="*60)
+print("📊 SESSION SUMMARY")
+print("="*60)
+print("Questions Asked :", questions_asked)
+print("Answers Found   :", answers_found)
+print("Not Found       :", not_found)
+print("="*60)
+print("👋 Thank you for using CodeAlpha Smart FAQ Chatbot!"
